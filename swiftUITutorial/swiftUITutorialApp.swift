9dc455e51059b7e894253b4c234c36dee2a8483d
kaikai <gamera1000x@gmail.com>
2023-10-07 15:45:22 +0900

    Initial Commit

---
+++ swiftUITutorial/swiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUITutorialApp.swift
+//  swiftUITutorial
+//
+//  Created by 青山凱 on 2023/10/07.
+//
+
+import SwiftUI
+
+@main
+struct swiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
