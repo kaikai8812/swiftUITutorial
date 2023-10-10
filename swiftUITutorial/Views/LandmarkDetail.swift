f271dc80c4a83d84db2c0360061d6cca49ad0c81
kaikai <gamera1000x@gmail.com>
2023-10-11 00:11:00 +0900

    Listから、Detail画面に遷移ができるようにする

---
+++ swiftUITutorial/Views/LandmarkDetail.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarkDetail.swift
+//  swiftUITutorial
+//
+//  Created by 青山凱 on 2023/10/10.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandmarkDetail()
+}
