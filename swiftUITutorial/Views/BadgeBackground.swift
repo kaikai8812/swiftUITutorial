48266c998ba8f585a161bcdf2840acbbaefde2ab
kaikai <gamera1000x@gmail.com>
2023-10-15 17:10:56 +0900

    Draw the badge background
    詳しい処理内容までは把握できていないが、swiftUIを用いて、複雑な図形も描画することができるっぽい

---
+++ swiftUITutorial/Views/BadgeBackground.swift
@@ -0,0 +1,18 @@
+//
+//  BadgeBackground.swift
+//  swiftUITutorial
+//
+//  Created by 青山凱 on 2023/10/15.
+//
+
+import SwiftUI
+
+struct BadgeBackground: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BadgeBackground()
+}
---
+++ swiftUITutorial/Views/HexagonParameters.swift
@@ -0,0 +1,8 @@
+//
+//  HexagonParameters.swift
+//  swiftUITutorial
+//
+//  Created by 青山凱 on 2023/10/15.
+//
+
+import Foundation
