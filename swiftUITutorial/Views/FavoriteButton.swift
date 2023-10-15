d51724f131cc0410b9701af245e70a1b8fcc0a45
kaikai <gamera1000x@gmail.com>
2023-10-15 14:50:22 +0900

    favoriteボタンのUIを作成

---
+++ swiftUITutorial/Views/FavoriteButton.swift
@@ -0,0 +1,18 @@
+//
+//  FavoriteButton.swift
+//  swiftUITutorial
+//
+//  Created by 青山凱 on 2023/10/15.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FavoriteButton()
+}
