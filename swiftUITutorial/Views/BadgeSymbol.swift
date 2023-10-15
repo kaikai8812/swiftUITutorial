d2e09089f55236af4be821215d115e9192ab1130
kaikai <gamera1000x@gmail.com>
2023-10-15 17:38:09 +0900

    バッジシンボルを作成する。

---
+++ swiftUITutorial/Views/BadgeSymbol.swift
@@ -0,0 +1,18 @@
+//
+//  BadgeSymbol.swift
+//  swiftUITutorial
+//
+//  Created by 青山凱 on 2023/10/15.
+//
+
+import SwiftUI
+
+struct BadgeSymbol: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BadgeSymbol()
+}
---
+++ swiftUITutorial/Views/RotatedBadgeSymbol.swift
@@ -0,0 +1,18 @@
+//
+//  RotatedBadgeSymbol.swift
+//  swiftUITutorial
+//
+//  Created by 青山凱 on 2023/10/15.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RotatedBadgeSymbol()
+}
