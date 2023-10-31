2ef81968ed5f092c961c773f90917fc9ee4d271f
kaikai <gamera1000x@gmail.com>
2023-11-01 00:57:21 +0900

    editButtonの作成

--- swiftUITutorial/Views/Profiles/ProfileHost.swift
+++ swiftUITutorial/Views/Profiles/ProfileHost.swift
@@ -8,12 +8,23 @@
 import SwiftUI
 
 struct ProfileHost: View {
+    @Environment(\.editMode) var editMode
+    @Environment(ModelData.self) var modelData
     @State private var draftProfile = Profile.default
-    
     var body: some View {
         Text("Profile for: \(draftProfile.username)")
         VStack(alignment: .leading, spacing: 20) {
-            ProfileSummary(profile: draftProfile)
+            
+            HStack {
+                Spacer()
+                EditButton()
+            }
+            
+            if editMode?.wrappedValue == .inactive {
+                ProfileSummary(profile: modelData.profile)
+            } else {
+                Text("Profile Editor")
+            }
         }
         .padding()
     }
@@ -21,4 +32,6 @@ struct ProfileHost: View {
 
 #Preview {
     ProfileHost()
+        .environment(ModelData())
+    //  このViewの子Viewである、ProfileSummaryがenvironmentで値を取得しているので、親Viewであるこいつも、environment修飾子が必要になる。
 }
