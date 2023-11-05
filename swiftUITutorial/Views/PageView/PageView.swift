f883d717ddf9a67f13088ebe0a4d4c341a0b20af
kaikai <gamera1000x@gmail.com>
2023-11-05 17:05:40 +0900

    PageViewから、PageVCに対して、値をバインディングする

--- swiftUITutorial/Views/PageView/PageView.swift
+++ swiftUITutorial/Views/PageView/PageView.swift
@@ -10,13 +10,18 @@ import SwiftUI
 struct PageView<Page: View>: View {
     
     var pages: [Page]
+    @State private var currentPage = 0
     
     var body: some View {
-        PageViewController(pages: pages)
+        VStack(content: {
+//            Button("次のページ") { currentPage += 1 }
+            PageViewController(pages: pages, currentPage: $currentPage)  //  PageViewControllerが、複数のVCを持って、画像を出しているイメージ
+            Text("Current Page: \(currentPage)")
+        })
     }
 }
 
 #Preview {
     PageView(pages: ModelData().features.map { FeatureCard(landmark: $0) })
-//        .aspectRatio(3/2, contentMode: .fill)
+        .aspectRatio(3/2, contentMode: .fit)
 }
--- swiftUITutorial/Views/PageView/PageViewController.swift
+++ swiftUITutorial/Views/PageView/PageViewController.swift
@@ -10,11 +10,12 @@ import UIKit
 
 struct PageViewController<Page: View>: UIViewControllerRepresentable {
     
+    var pages: [Page]
+    @Binding var currentPage: Int
+    
     func makeCoordinator() -> Coordinator {
         Coordinator(self)
     }
-    
-    var pages: [Page]
     
     func makeUIViewController(context: Context) -> UIPageViewController {
         let pageViewController = UIPageViewController(
@@ -22,19 +23,20 @@ struct PageViewController<Page: View>: UIViewControllerRepresentable {
             navigationOrientation: .horizontal)
         
         pageViewController.dataSource = context.coordinator
+        pageViewController.delegate = context.coordinator
         
         return pageViewController
     }
     
     func updateUIViewController(_ pageViewController: UIPageViewController, context: Context) {
         pageViewController.setViewControllers(
-            [context.coordinator.controllers[1]], direction: .forward, animated: true)
+            [context.coordinator.controllers[currentPage]], direction: .forward, animated: true)
     }
     
     //  Coordinator側が、controllersを持つようになり、スワイプ時に、どのような動作をするべきかのロジック(ページ遷移まわりのロジック を保持するようになる。
     //  UIPageViewControllerDataSourceのプロトコルに準拠し、coordinator自体がdataSourceになることによって、pageViewControllerのdatasourceに情報を渡せる。
     
-    class Coordinator: NSObject, UIPageViewControllerDataSource {
+    class Coordinator: NSObject, UIPageViewControllerDataSource, UIPageViewControllerDelegate {
         var parent: PageViewController
         var controllers = [UIViewController]()
         
@@ -69,6 +71,18 @@ struct PageViewController<Page: View>: UIViewControllerRepresentable {
             }
             return controllers[index + 1]
         }
+        
+        func pageViewController(
+            _ pageViewController: UIPageViewController,
+            didFinishAnimating finished: Bool,
+            previousViewControllers: [UIViewController],
+            transitionCompleted completed: Bool) {
+                if completed,
+                   let visibleViewController = pageViewController.viewControllers?.first,
+                   let index = controllers.firstIndex(of: visibleViewController) {
+                    parent.currentPage = index
+                }
+            }
     }
     
 }
